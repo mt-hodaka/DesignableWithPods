494d1819554aeeda38a762ab8ed196dc04cdc43d
mt.hodaka <mt.hodaka7@gmail.com>
2019-09-07 15:15:09 +0900

    create project

---
+++ DesignableWithPods/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesignableWithPods
+//
+//  Created by hodaka fukushima on 2019/09/07.
+//  Copyright © 2019 mt.hodaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
